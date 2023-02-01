1d932d25e918fb3e0a4697834545667e2f3bda95
David Mead <2652016m@student.ac.uk>
2023-02-01 11:46:28 +0000

    up to pg 157

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from rango.models import Page, Category
+
+from rango.models import Page, Category, UserProfile
+from rango.models import UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.NAME_MAX, help_text="Please enter the category name.")
@@ -7,9 +9,7 @@ class CategoryForm(forms.ModelForm):
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
-    # An inline class to provide additional information on the form.
     class Meta:
-    # Provide an association between the ModelForm and a model
         model = Category
         fields = ('name',)
 
@@ -22,13 +22,6 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude = ('category',)
-        # What fields do we want to include in our form?
-        # This way we don't need every field in the model present.
-        # Some fields may allow NULL values; we may not want to include them.
-        # Here, we are hiding the foreign key.
-        # we can either exclude the category field from the form,
-        # or specify the fields to include (don't include the category field).
-        #fields = ('title', 'url', 'views')
 
         def clean(self):
             cleaned_data = self.cleaned_data
@@ -38,3 +31,15 @@ class PageForm(forms.ModelForm):
                 cleaned_data['url'] = url
         
             return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username','email','password',)
+
+class UserProfileForm(form.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website','picture')
